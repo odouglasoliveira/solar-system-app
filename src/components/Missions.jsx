7fc38bcc4b61059c00b1cbf9c9fb062b87cf131c
Douglas Oliveira <doglasolivera13@gmail.com>
2023-03-02 13:42:54 -0300

    Estilização da Seção Missões

--- src/components/Missions.jsx
+++ src/components/Missions.jsx
@@ -6,18 +6,20 @@ import MissionCard from './MissionCard';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <>
         <Title headline="Missões" />
-        {missions.map((mission) => (
-          <MissionCard
-            key={ mission.name }
-            name={ mission.name }
-            year={ mission.year }
-            country={ mission.country }
-            destination={ mission.destination }
-          />
-        ))}
-      </div>
+        <div className="missions" data-testid="missions">
+          {missions.map((mission) => (
+            <MissionCard
+              key={ mission.name }
+              name={ mission.name }
+              year={ mission.year }
+              country={ mission.country }
+              destination={ mission.destination }
+            />
+          ))}
+        </div>
+      </>
     );
   }
 }
